1a400812c20442eb2b42d559b549b0643e21b6f3
unknown <tolotraras19@gmail.com>
2025-06-18 11:42:24 +0300

    aaaaa

--- src/Ticketing/urls.py
+++ src/Ticketing/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #Fitia nantenaina
+#tolotra
 
 from django.contrib import admin
 from django.urls import path
